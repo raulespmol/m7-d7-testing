d70156a8ab0ae63b05d60edad7d7e1265a052503
Raul Espinoza M <raul.espmol@gmail.com>
2024-06-12 22:48:20 -0400

    test GET /cafes

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -2,5 +2,12 @@ const request = require("supertest");
 const server = require("../index");
 
 describe("Operaciones CRUD de cafes", () => {
+  it("Ruta GET /cafes", async () => {
+    const response = await request(server).get('/cafes').send()
+    const {statusCode, body} = response
 
+    expect(statusCode).toBe(200)
+    expect(body).toBeInstanceOf(Array)
+    expect(body.length).toBeGreaterThan(0)
+  })
 });
